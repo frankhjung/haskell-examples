84c3540da578c48938e6fddb31da23d9ecd77365
Frank Jung <frankhjung@linux.com>
2024-02-23 15:17:45 +1100

    add io example to write and read file

--- src/ShowFile.hs
+++ src/ShowFile.hs
@@ -1,7 +1,7 @@
 {-|
 
 Module      : ShowFile
-Description : Effective Haskell exercise.
+Description : Effective Haskell exercises, Chapter 7 Understanding IO.
 Copyright   : © Frank Jung, 2024
 License     : GPL-3.0-only
 
@@ -10,6 +10,9 @@ License     : GPL-3.0-only
 module ShowFile (
     noPasswd
   , showFile
+  , makeAndReadFile
+  , makeAndShow
+  , safeIO
   ) where
 
 -- | Show path except if `/etc/passwd`.
@@ -22,3 +25,22 @@ noPasswd path
 -- | Show file content.
 showFile :: FilePath -> IO String
 showFile = noPasswd
+
+-- | Make and read a file.
+-- Write and then read a file.
+makeAndReadFile :: Int -> IO String
+makeAndReadFile fnumber =
+  let fname = "/tmp/test-" <> show fnumber
+  in writeFile fname fname >> readFile fname
+
+-- | Make and show file helper function.
+-- Write and read a file.
+makeAndShow :: Int -> IO ()
+makeAndShow n = makeAndReadFile n >>= putStrLn
+
+-- | Test writing and reading a number of files.
+-- Efficient sequencing of IO actions ensuring file handle is closed.
+safeIO :: Int -> IO ()
+safeIO n
+  | n > 0 && n < 100000 = mapM_ makeAndShow [1..n]
+  | otherwise = error "Must be a positive number between 1 and 100,000."
